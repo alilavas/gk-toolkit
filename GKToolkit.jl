67c2861bda774273bff4adab470ecd22c3c5d1b7
AliLavas <h.lavasani@gmail.com>
2021-02-13 20:22:40 -0500

    removing z2 linear algebra functions

--- GKToolkit.jl
+++ GKToolkit.jl
@@ -2,7 +2,7 @@ using LinearAlgebra;
 using Random;
 using Distributed
 using JLD2, FileIO, StatsBase, Dates
-
+include("z2algebra.jl")
 
 
 #-------------------------------------------------
@@ -47,49 +47,6 @@ function measurestabilizer!(stab,g,stabsign=nothing)
     stabX[pivotindex,:]=gX
     stabZ[pivotindex,:]=gZ
 
-    return
-end
-#-------------------------------------------------
-#-------------------------------------------------
-function rank2!(A)
-    m,n=size(A)
-    lastpivotcol=0
-
-    for i in 1:m
-        for j in (lastpivotcol+1):n
-            if A[i,j]!=0
-                for jj in (j+1):n
-                    if A[i,jj]!=0
-                        A[i:end,jj].⊻=view(A,i:end,j)
-                    end
-                end
-                z2swapcol!(A,j,lastpivotcol+1,i)
-                lastpivotcol+=1
-                break
-            end
-        end
-    end
-
-    return lastpivotcol
-end
-#-------------------------------------------------
-#-------------------------------------------------
-function z2swaprow!(A,i,j,leftcol=1)
-    if i!=j
-        A[i,leftcol:end].⊻=view(A,j,leftcol:end)
-        A[j,leftcol:end].⊻=view(A,i,leftcol:end)
-        A[i,leftcol:end].⊻=view(A,j,leftcol:end)
-    end
-    return
-end
-#-------------------------------------------------
-#-------------------------------------------------
-function z2swapcol!(A,i,j,toprow=1)
-    if i!=j
-        A[toprow:end,i].⊻=view(A,toprow:end,j)
-        A[toprow:end,j].⊻=view(A,toprow:end,i)
-        A[toprow:end,i].⊻=view(A,toprow:end,j)
-    end
     return
 end
 #-------------------------------------------------
@@ -208,33 +165,7 @@ function rowecholen2_cm!(A)
 
     return lastPivotCol,transpose(B)[:,n+1:end]
 end
-#-------------------------------------------------
-#-------------------------------------------------
-function gaussianelimination_col(A)
-    m,n=size(A)
-    B=vcat(A,Matrix{Uint8}(I,n,n))
-    pivots=[]
-    lastPivotCol=0
 
-    for i in 1:m
-        for j in (lastPivotCol+1):n
-            if B[i,j]!=0
-                push!(pivots,(i,lastPivotCol+1))
-                for jj in (j+1):n
-                    if B[i,jj]!=0
-                        B[i:end,jj].⊻=B[i:end,j]
-                    end
-                end
-                z2SwapCol!(B,j,lastPivotCol+1,i)
-                lastPivotCol+=1
-                break
-            end
-        end
-    end
-
-    rank=lastPivotCol
-    return B[1:m,:],rank,pivots,B[m+1:end,:]
-end
 #-------------------------------------------------
 #-------------------------------------------------
 function gaussianelimination_row(A)
