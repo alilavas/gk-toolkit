a82db1a142e65e8151d5a6145c0bef21710a72c5
alilavas <h.lavasani@gmail.com>
2022-05-26 16:45:21 -0400

    added forceMeasure function

--- GKA.jl
+++ GKA.jl
@@ -102,8 +102,7 @@ function measure(state::StateDensityMatrix,g,gphase=0x00,verbose=false)
     verbose && printstyled("\n measuring: $g\n";color=:blue)
     verbose && println("state has originally tableau:")
     verbose && display(state.tab)
-    verbose && println("with ranke:$(state.rank) and phases:$(state.phases)")
-    #measure g on the state. If outcome is true, gphase should have been provided
+    verbose && println("with rank:$(state.rank) and phases:$(state.phases)")
     
     n=size(state.tab,1)÷2
     r=state.rank
@@ -228,6 +227,156 @@ function measure(state::StateDensityMatrix,g,gphase=0x00,verbose=false)
     end
 end
 #-------------------------------------------------
+#-------------------------------------------------
+
+function forceMeasure(state,g,gphase=0x00,verbose=false)
+#     the only deference between this function and measure is:
+#     1) the phase of g will not be randomized when g is not in the stabilizer group.
+#     2) if -gphase*g is in the stabilizer group, it kills the state (sets it to zero) 
+
+
+    
+    verbose && printstyled("\n projecting on: $g\n with phase:$gphase ";color=:blue)
+    verbose && println("state's tableau:")
+    verbose && display(state.tab)
+    verbose && println("rank:$(state.rank), phases:$(state.phases)")
+
+    
+    n=size(state.tab,1)÷2
+    r=state.rank
+    noncommuting=findall(x->x==0x01,commute(state.tab[:,1:r],g))
+
+    if length(noncommuting)==0
+        verbose && println("no noncommuting in-group stabilizer")
+        #check whether g is in the stabilizer group
+        
+        noncommuting_S=findall(x->x==0x01,commute(state.tab[:,r+1:n],g)).+r
+        if length(noncommuting_S)!=0
+            verbose && println("a non-commuting out-of-group stabilizer has been found:\n $(state.tab[:,noncommuting_S[1]])")
+
+            #a non-trivial operator has been measured. 
+            
+            pivotIdx=noncommuting_S[1]
+            
+            #after this loop we have a valid table with a single stabilizer that anti-commutes with g
+            for j in noncommuting_S[2:end]
+                addCols(state,pivotIdx,j)
+#                 the following line is not necassary because the destabilizer for pivotIdx is going to be descarded
+#                 addCols(state,j+n,pivotIdx+n)     
+            end
+
+#             since it is forced measurement
+#             outcome=rand([0x00,0x02])
+            outcome=0x00
+            
+            insertStabilizer(state,g,(outcome+gphase)%0x04,pivotIdx)
+            updateDestabilizers(state,pivotIdx)
+                        
+            swapCols(state,pivotIdx,r+1)
+            swapCols(state,pivotIdx+n,r+1+n)
+            
+            state.rank=r+1
+            return outcome
+        end
+        verbose && println("no noncommuting out-of-group stabilizer")
+        
+        noncommuting_D=findall(x->x==0x01,commute(state.tab[:,r+1+n:2n],g)).+(r+n)
+        if length(noncommuting_D)!=0
+            verbose && println("a non-commuting out-of-group destabilizer has been found:\n $(state.tab[:,noncommuting_D[1]])")
+#             a non-trivial operator has been measured. 
+#             we swap the stabilizer and destabilizer for the first non-commuting destabilizer
+#             By doing so, we arrive at a valid tableau and a single stabilizer that anti-commutes with g
+            swapCols(state,noncommuting_D[1],noncommuting_D[1]-n)
+            pivotIdx=noncommuting_D[1]-n
+            
+#             since it is forced measurement
+#             outcome=rand([0x00,0x02])
+            outcome=0x00
+            
+            insertStabilizer(state,g,(outcome+gphase)%0x04,pivotIdx)
+            updateDestabilizers(state,pivotIdx)
+            
+            swapCols(state,pivotIdx,r+1)
+            swapCols(state,pivotIdx+n,r+1+n)            
+            
+            state.rank=r+1
+            return outcome
+        end
+        verbose && println("no noncommuting out-of-group destabilizer.\n g is in the stabilizer group")
+
+        
+#         +-g is in the stabilizer group, so the measurement outcome is deterministic and rank remains the same
+        noncommuting_DS=findall(x->x==0x01,commute(state.tab[:,1+n:r+n],g))
+        if length(noncommuting_DS)==0
+            if sum(g)==0
+                return 1
+            else
+                printstyled("ERROR: SOMETHING IS VERY WRONG: g commutes with everything!\n";color = :red)
+                println("g:")
+                display(g)
+                println("tab:")
+                display(state.tab)
+                println("rank:$(state.rank)")
+                return
+            end
+        end
+        ph=state.phases[noncommuting_DS[1]]
+        op=state.tab[:,noncommuting_DS[1]]
+        
+        for i in noncommuting_DS[2:end]
+            ph=ph+state.phases[i]+productsign(op,state.tab[:,i])
+            op=op.⊻state.tab[:,i]
+        end
+        ph=ph%0x04
+        
+        if g!=op
+            
+            printstyled("ERROR: SOMETHING IS VERY WRONG:\n $g \n is not equal to\n $op\n";color = :red)
+            println("g:")
+            display(g)
+            println("tab:")
+            display(state.tab)
+            println("rank:$(state.rank)")
+        end
+        if gphase==ph
+            return 0x00
+        elseif gphase==(ph+0x02)%0x04
+            printstyled("Warning: THE STATE HAD NO AMPLITITUDE IN THE FORCED SUBSPACE.\n";color = :red)
+            state=0
+            return 0x00
+        else
+            printstyled("ERROR: SOMETHING IS VERY WRONG. MAYBE YOU ARE MEASURING AN ANTI-HERMITIONA OPERATOR\n";color = :red)
+            return 
+        end
+            
+    else
+        verbose && println("g anti-commutes with a stabilizer: $(state.tab[:,noncommuting[1]])")
+        #the measurement is not compatible with the stabilizer group. the outcome is random
+        pivotIdx=noncommuting[1]
+        for j in noncommuting[2:end]
+            addCols(state,pivotIdx,j)
+#             the following line is not necassary because the destabilizer for pivotIdx is going to be descarded
+#             addCols(state,j+n,pivotIdx+n)     
+        end
+        
+        #update the out-of-group stabilizers, so tehy commute with the newly added stabilizer
+        for j in findall(x->x==0x01,commute(state.tab[:,r+1:n],g)).+r
+            addCols(state,pivotIdx,j)
+#             the following line is not necassary because the destabilizer for pivotIdx is going to be descarded
+#             addCols(state,j+n,pivotIdx+n)     
+        end
+
+#         since it is forced measurement
+#         outcome=rand([0x00,0x02])
+        outcome=0x00
+        
+        insertStabilizer(state,g,(outcome+gphase)%0x04,pivotIdx)
+        updateDestabilizers(state,pivotIdx)
+        return outcome
+    end
+end
+
+#-------------------------------------------------
 #-------------------------------------------------       
 function totallyMixed(n)
     state=StateDensityMatrix(Matrix{UInt8}(I,2n,2n),zeros(UInt8,2n),0)
@@ -306,6 +455,4 @@ function decohereX(state,i)
         return 0x01
     end
 end
-
-
 
